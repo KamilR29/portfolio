77c7b182c87e00fdff2808b5c79a1ef86cd5d19c
Kamil <kamil.raczkowski@onet.pl>
2023-12-19 20:09:33 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import avatar from './image/hmmm.png'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <div className="Avatar">
+          <img className="Avatar-icon" src={avatar} alt="It is avtar icon" />
+        </div>
+
+      </div>
+
     </div>
   );
 }
